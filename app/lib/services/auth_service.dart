d29b9e8d9da7bdfef4a169b8a9213bd7288c2926
jorget10231-arch <jorget10231@gmail.com>
2026-08-31 15:14:15 -0500

    refactor: separar servicio de autenticacion

---
+++ app/lib/services/auth_service.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  AuthService({FirebaseAuth? auth}) : _auth = auth ?? FirebaseAuth.instance;
+
+  final FirebaseAuth _auth;
+
+  User? get currentUser => _auth.currentUser;
+
+  Stream<User?> get authStateChanges => _auth.authStateChanges();
+
+  Future<UserCredential> signIn({
+    required String email,
+    required String password,
+  }) {
+    return _auth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  Future<void> signOut() => _auth.signOut();
+}
