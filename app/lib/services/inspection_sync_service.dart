24b420b790fdbe9d7c2980e05c3138be9e5cfb12
jorget10231-arch <jorget10231@gmail.com>
2026-08-31 23:25:22 -0500

    chore: limpiar import no utilizado

--- app/lib/services/inspection_sync_service.dart
+++ app/lib/services/inspection_sync_service.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-import '../models/inspection.dart';
 import '../repositories/local_inspection_repository.dart';
 
 class InspectionSyncService {
