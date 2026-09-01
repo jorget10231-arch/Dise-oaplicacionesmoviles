6c09dba3e23bececde56f26134d97ee722141a25
jorget10231-arch <jorget10231@gmail.com>
2026-08-31 23:25:15 -0500

    test: corregir import de MaterialApp

--- app/test/widget_test.dart
+++ app/test/widget_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:safeinspect_mobile/main.dart';
 import 'package:safeinspect_mobile/screens/home/home_page.dart';
