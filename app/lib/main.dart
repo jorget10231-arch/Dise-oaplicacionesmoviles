cc278deeb1d1a672035116540f7a4b5c27bc8f76
jorget10231-arch <jorget10231@gmail.com>
2026-08-31 23:03:45 -0500

    feat: integrar Firebase y plataforma Android

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,10 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'firebase_options.dart';
 import 'core/app_theme.dart';
 import 'screens/home/home_page.dart';
 import 'screens/login/login_page.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const SafeInspectApp());
 }
 
